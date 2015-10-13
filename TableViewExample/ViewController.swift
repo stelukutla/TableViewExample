dc201cc4e9255e715e04830f95fcd4646688a602
Sankara Telukutla <sankara.telukutla@gmail.com>
2015-10-13 00:07:41 -0700

    Initial files

--- TableViewExample/ViewController.swift
+++ TableViewExample/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate{
+    
+    var nameArray = ["Shankar", "Praneel", "Swetha", "Neel", "Reddy", "Bob", "Tilak", "Dimon", "Raj", "Modi"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default,reuseIdentifier: "myCell")
+        
+        cell.textLabel!.text=nameArray[indexPath.row]
+        
+        return cell
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
